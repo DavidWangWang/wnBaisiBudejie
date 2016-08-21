0cd4311b878609235c64e6fb9694fd439683f3d1
wangning-lingli <351723770@qq.com>
2016-08-21 10:25:35 +0800

    hahah

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by 王宁 on 16/8/21.
+//  Copyright © 2016年 王宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by 王宁 on 16/8/21.
+//  Copyright © 2016年 王宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
